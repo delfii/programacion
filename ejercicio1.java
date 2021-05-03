62747cff739c76da47b9c5a8f2417c25d1e9fe1d
delfii <81364432+delfii@users.noreply.github.com>
2021-05-03 10:39:03 -0300

    Add files via upload

---
+++ ejercicio1.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author pc01
+ */
+public class ejercicio1 {
+    private String nombre;
+    private int edad;
+    private String raza;
+    public ejercicio1 (String nombre, int edad, String raza){
+        this.nombre=nombre;
+        this.edad=edad;
+        this.raza=raza;
+        public String getnombre(){
+            return (this.nombre);}
+        
+        public void setnombre (){
+            this.nombre=nombre;
+        }
+        public int getedad (){
+            return(this.edad);}
+        public void setedad (int edad){
+            this.edad=edad;}
+        public void setraza(){
+            this.raza=raza;}
+        public String getraza(){
+            return (this.raza);}
+        }
+  Ejercicio1 labrador []= new ejercicio1[a];
+  for (int i=0; i<labrador.leight;i++){
+labrador [i]newejercicio1();
+labrador[i].setnombre(JOptionpane.ShowImputDialog("ingrese el nombre"))
+labrador[i].setedad(JOptionpane.ShowImputDialog("ingrese la edad"))
+
+
+
+
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+}
